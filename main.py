ef82cd37c936846639c317216796f34d24cb73b5
EgorDubovik <posik.web.m@gmail.com>
2021-12-28 20:18:51 -0600

    load image

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    print('Hello')
+    img = cv2.imread('test.png', cv2.IMREAD_COLOR)
+    print(img[50, 50])
+    cv2.imshow('Test', img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 
